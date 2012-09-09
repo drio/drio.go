3778ad18f80caeb1866274e211f8ad99416f51c8
David Rio Deiros <driodeiros@gmail.com>
2012-09-09 07:21:39 -0500

    * Allow running format from anywhere

--- format.sh
+++ format.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "cd $DIR"
 for g in `find . -name "*.go"`
 do
   echo "gofmt -tabs=false -tabwidth=2 $g > $g.fix"
   echo "mv $g.fix $g"
 done
+echo "cd - > /dev/null"
