2a5217fef8b388b3cd5cdde99248009193f15f8c
David Rio Deiros <driodeiros@gmail.com>
2012-09-15 21:08:49 -0500

    * Auto-fetch next euler problem.

---
+++ cli/euler/new.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+def getDesc(n)
+  start = false
+  desc = []
+  `lynx -dump http://projecteuler.net/problem=#{n}`.each_line do |l|
+    start = true  if l =~ /^Problem\s/
+    start = false if l =~ /^References/
+    desc << "// " + l.chomp if start
+  end
+  desc.join("\n")
+end
+
+n = []
+Dir["p*"].each {|d| n << d.match(/(\d+)$/)[1]}
+nextNum = n.sort[-1].to_i + 1
+dir = "./p#{nextNum}"
+
+`mkdir #{dir}`
+File.open(dir + "/euler-p#{nextNum}.go", "w") do |f|
+  f.puts DATA.read.gsub(/__DESC__/, getDesc(nextNum))
+end
+
+__END__
+package main
+
+import "fmt"
+
+__DESC__
+func main() {
+}
